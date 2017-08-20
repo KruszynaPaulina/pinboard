f7496f653ea5c9651e10b9ec66129120c0074efe
KruszynaPaulina <kruszyna.paulina@gmail.com>
2017-08-20 20:13:46 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'git.heroku.com/stormy-river-88912.git' }
+  config.action_mailer.default_url_options = { host: 'mighty-falls-12286.herokuapp.com' }
 end
